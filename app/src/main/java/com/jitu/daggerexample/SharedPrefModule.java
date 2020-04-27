13344e6245ade38c0bfca83e75ce53cd3ef5ec1a
JitenderChawla <jitenderchawla4910@gmail.com>
2020-04-27 22:39:22 +0530

    In this SharedPreference instance is passed with Dagger2
    In this commit we are passing sharedpreference instance using dagger2

---
+++ app/src/main/java/com/jitu/daggerexample/SharedPrefModule.java
@@ -0,0 +1,31 @@
+package com.jitu.daggerexample;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class SharedPrefModule {
+    private Context context;
+
+    public SharedPrefModule(Context _context){
+        this.context = _context;
+    }
+
+    @Singleton
+    @Provides
+    public Context provideContext(){
+        return context;
+    }
+
+    @Singleton
+    @Provides
+    public SharedPreferences provideSharedPreferences(Context _context){
+        return PreferenceManager.getDefaultSharedPreferences(_context);
+    }
+}
